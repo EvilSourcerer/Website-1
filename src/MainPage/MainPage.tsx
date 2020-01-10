aea67f27f539ffc7926e93746a664e7b9536d859
EvilSourcerer <evilsourcerer@gmail.com>
2020-01-10 03:39:45 -0500

    add info section

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -4,13 +4,21 @@ import { Typography, Fab, Theme } from '@material-ui/core';
 import { ViewCompact as DashboardIcon, MoneyOff as FreeIcon } from '@material-ui/icons';
 
 import topography from './topography.svg';
+import { AlteningDark } from '../theme/AlteningDark';
 
 const mainPageStyles = makeStyles((theme: Theme) => ({
     jumbotron: {
         backgroundImage: `url(${topography})`,
+        backgroundColor: AlteningDark[600],
         clipPath: 'polygon(0 0, 1600px 0, 1600px 87%, 0 100%)',
         height: '800px',
         width: '100%'
+    },
+    moreInfo: {
+        paddingTop: theme.spacing(2),
+        paddingLeft: '30%',
+        paddingRight: '30%',
+        textAlign: 'center'
     },
     jumboTitle: {
         paddingTop: '80px',
@@ -47,6 +55,49 @@ const MainPage: React.FC = () => {
                         </Fab>
                     </div>
                 </div>
+            </div>
+            <div className={classes.moreInfo}>
+                <Typography variant='h3'>About</Typography>
+                <Typography variant='body1'>
+                    <b>How Is The Altening Unique?</b>
+                </Typography>
+                <Typography variant='body1' color='textSecondary'>
+                    TheAltening is one of the best minecraft account generators on the market.
+                    <br />
+                    <br />
+                    <b>
+                        It's not like a regular minecraft alt generator and uses a special system called “Tokens” which
+                        differentiate from most generators giving you just the email:password of an alt.
+                    </b>
+                    <br />
+                    Using our system, you’ll be able to generate Tokens which you can use in your minecraft client to
+                    login to the accounts.
+                    <br />
+                    <br />
+                    Note: you won’t be provided with an email:password but instead with a Token which is a much better
+                    form of authentication.
+                </Typography>
+                <br />
+                <br />
+                <Typography variant='body1'>
+                    <b>Why Are Tokens Superior?</b>
+                </Typography>
+                <br />- A Simpler format, you can use any password
+                <br />- Each account is checked before being generated
+                <br />- No more dead alts
+                <br />- No more mojang blocks
+                <br />- Allows us to give out more accounts
+                <br />
+                <br />
+                <Typography variant='body1'>
+                    <b>
+                        TheAltening currently only supports Windows operating systems, and won’t be available for Mac or
+                        Linux based OS’s.
+                    </b>
+                    To use TheAltening without API-based applications, you will need to download our
+                    <b> custom authenticator and switch to our servers.</b> From there you will be able to enter the
+                    Tokens into your Minecraft launcher just like a normal login and it will let you use your new alt!
+                </Typography>
             </div>
         </>
     );
